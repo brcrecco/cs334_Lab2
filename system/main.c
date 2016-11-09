5a8af9ecb2587b271dfd6a8da3c495b27c069876
brcrecco@vassar.edu <brcrecco@dhcp-pool-238.cs.vassar.edu>
2016-11-08 21:49:19 -0500

    Implementing task3, non working

--- system/main.c
+++ system/main.c
@@ -3,14 +3,28 @@
 #include <xinu.h>
 #include "lab2.h"
 /*DO NOT DEFINE DEPENDENT VARIABLES HERE*/
-int consumed = 0;
-int produced = 0;
+int task2consumed = 0; // task 2 consumed message count
+int task2produced = 0; // task 2 produced message count
 
-process Task2Test(), Task2Producer(), Task2Consumer();
+int task3consumed1 = 0; // task 3 consumed messages
+int task3consumed2 = 0;
+int task3consumed3 = 0;
+
+int task3produced1 = 0; // task 3 produced messages
+int task3produced2 = 0;
+int task3produced3 = 0;
+
+int allocated1 = 0; // task 3 allocated messages
+int allocated2 = 0;
+int allocated3 = 0;
+
+
+process Task2Test(), Task2Producer(), Task2Consumer(), Task3Producer(), Task3Consumer(), Task3Test(),
+		Task3Allocator();
 
 process	main(void)
 {
-	resume(create(Task2Test, 8192, 50, "Task2Test", 0));
+	resume(create(Task3Test, 8192, 50, "Task3Test", 0));
 	return OK;
 }
 
@@ -22,8 +36,7 @@ process Task2Test(void) {
 	resume(consumer);
 	
 	while(1) {
-		//sleepms(1);
-		kprintf("Producer created: %d items and Consumer consumed: %d items\n", produced, consumed);
+		kprintf("Producer created: %d items and Consumer consumed: %d items\n", task2produced, task2consumed);
 	}
 
 	return OK;
@@ -35,7 +48,7 @@ process Task2Producer(pid32 consumer) {
 	while(1) {
 		uint32 sent_msgs = SendMsg(consumer, msg, 1);
 		if(sent_msgs != SYSERR) {
-			produced += sent_msgs;
+			task2produced += sent_msgs;
 		}	
 		sleepms(1);
 	}
@@ -50,10 +63,142 @@ process Task2Consumer(void) {
 	while(1) {
 		syscall rcv = RcvMsg(msg, numMsgs);
 		if(rcv == OK) {
-			consumed += numMsgs;
+			task2consumed += numMsgs;
 		}
 		sleepms(1);
 	}
 
+	return OK;
+}
+
+process Task3Test(void) {
+
+	umsg32* msg1 = 1;
+	umsg32* msg2 = 2;
+	umsg32* msg3 = 3;
+
+	pid32 consumer1 = create(Task3Consumer, 8192, 20, "Task3Consumer1", 0);
+	pid32 consumer2 = create(Task3Consumer, 8192, 20, "Task3Consumer2", 0);
+	pid32 consumer3 = create(Task3Consumer, 8192, 20, "Task3Consumer3", 0);
+	pid32 allocator = create(Task3Allocator, 8192, 40, "Task3Allocator", 3, consumer1, consumer2, consumer3);
+	pid32 producer1 = create(Task3Producer, 8192, 20, "Task3Producer", 2, allocator, msg1);
+	pid32 producer2 = create(Task3Producer, 8192, 20, "Task3Producer", 2, allocator, msg2);
+	pid32 producer3 = create(Task3Producer, 8192, 20, "Task3Producer", 2, allocator, msg3);
+
+	pri16 p1 = resume(producer1);
+	pri16 p2 = resume(producer2);
+	pri16 p3 = resume(producer3);
+	pri16 a = resume(allocator);
+	pri16 c1 = resume(consumer1);
+	pri16 c2 = resume(consumer2);
+	pri16 c3 = resume(consumer3);
+
+	kprintf("p1: %d, p2: %d, p3: %d, a: %d, c1: %d, c2: %d, c3: %d \n", p1, p2, p3, a, c1, c2, c3);
+
+	while(1) {
+		kprintf("Producer 1 created: %d items and Consumer 1 consumed: %d items\n", task3produced1, task3consumed1);
+		kprintf("Producer 2 created: %d items and Consumer 2 consumed: %d items\n", task3produced2, task3consumed2);
+		kprintf("Producer 3 created: %d items and Consumer 3 consumed: %d items\n", task3produced3, task3consumed3);
+		kprintf("Allocator node has: %d items from 1 %d items from 2 %d items from 3\n", allocated1, allocated2, allocated3);
+
+		sleepms(1);
+	}
+
+	return OK;
+
+}
+
+process Task3Producer(pid32 allocator, umsg32* msg) {
+
+	kprintf("Reaches Producer \n");
+
+	while(1) {
+		uint32 sent_msgs = SendMsg(allocator, msg, 1);
+		if(sent_msgs != SYSERR) {
+			switch(*msg) {
+				case 1: task3produced1 += sent_msgs;
+						break;
+				case 2: task3produced2 += sent_msgs;
+						break;
+				case 3: task3produced3 += sent_msgs;
+						break;
+				default: kprintf("not producing\n");
+						break;
+			}
+		}
+		kprintf("sent_msgs = %d\n", sent_msgs);	
+		sleepms(1);
+	}
+
+	return OK;
+}
+
+process Task3Consumer(void) {
+
+	kprintf("Reaches Consumer \n");
+
+	umsg32* msg[1];
+	while(1) {
+		syscall rcv = RcvMsg(msg, 1);
+		if(rcv == OK) {
+			switch((*msg)[1]) {
+				case 1: task3consumed1++;
+						break;
+				case 2: task3consumed2++;
+						break;
+				case 3: task3consumed3++;
+						break;
+				default: kprintf("not consuming\n"); 
+						break;
+			}
+		}
+		sleepms(1);
+	}
+
+	return OK;
+}
+
+process Task3Allocator(pid32 consumer1, pid32 consumer2, pid32 consumer3) {
+	kprintf("Reaches Allocator \n");
+
+	umsg32* msg[5];
+
+	while(1) {
+		RcvMsg(msg, 5);
+		for(int i = 0; i < 5; i++){
+			switch((*msg)[i]) {
+				case 1: allocated1++;
+						break;
+				case 2: allocated2++;
+						break;
+				case 3: allocated3++;
+						break;
+				default: kprintf("alo not rcving legit\n"); 
+						break;
+			}
+		}
+
+		for(int i = 0; i < 5; i++) {
+			switch((*msg)[i]){
+				case 1: SendMsg(consumer1, msg[i], 1);
+						allocated1--;
+						break;
+
+				case 2: SendMsg(consumer2, msg[i], 1);
+						allocated2--;
+						break;
+
+				case 3: SendMsg(consumer3, msg[i], 1);
+						allocated3--;
+						break;
+
+				default: kprintf("alo not snding msgs \n"); 
+						break;
+
+
+			}
+		}
+		sleepms(10);
+	}
 	return OK;
 }
